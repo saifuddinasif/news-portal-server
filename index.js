ad8c56b894ee2ce529055d22c7b3c444caaefc8f
My Name <mdarfinji45@gmail.com>
2022-10-20 14:04:45 +0800

    some change

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
-
-const express = require('express')
-
-const app = express()
-
-const cors = require('cors')
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const categories = require('./categories.json');
+const news = require('./news.json');
 
-const port = process.env.PORT || 5000;
-const categories =require('./categories.json');
 app.get('/news-categories',(req,res) => {
-
-    
+  
     res.send(categories);
-
 })
+
+
+app.get('/category/:id', (req,res) => {
+
+     const id = req.params.id;
+
+     if(id=== '08'){
+
+        res.send(news)
+     }
+        else{
+            const category_news = news.filter(n => n.category_id ===id);
+            res.send(category_news)
+        }
+})
+
+app.get('/news/:id', (req, res) => { 
+    
+    
+      const id = req.params.id;
+
+      const selectedNews =  news.find(n=> n._id===id)
+
+      res.send(selectedNews)
+});
+
 
 app.get('/',(req,res) =>  {
 
     res.send('news runnig')
 }) 
-
 
 app.listen(port, () => {
 
